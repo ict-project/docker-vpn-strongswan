88d9fc21323602bbf617810980dbc5ba88feaac9
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2022-04-20 18:20:34 +0200

    docker secret

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+SECRET_PREFIX="$1"
+SECRET_SUFFIX="$2"
+SECRET_PREFIX_PK="${SECRET_PREFIX}private-key${SECRET_SUFFIX}"
+SECRET_PREFIX_PC="${SECRET_PREFIX}public-cert${SECRET_SUFFIX}"
+SECRET_PREFIX_DN="${SECRET_PREFIX}public-dn${SECRET_SUFFIX}"
+SECRET_PREFIX_SE="${SECRET_PREFIX}secrets${SECRET_SUFFIX}"
+PLACEHOLDER="# Placeholder for server"
+
 CA_PRIVATE_KEY="ca-key.pem"
 CA_PUBLIC_CERT="ca-cert.pem"
 SERVER_PRIVATE_KEY="server-key.pem"
@@ -20,9 +28,32 @@ if [[ ! -f "Dockerfile" ]]; then
     exit 2
 fi
 
+if [[ "PREFIX$SECRET_PREFIX" == "PREFIX" ]]; then
+    test -f "$SERVER_PRIVATE_KEY.secret" && mv -f "$SERVER_PRIVATE_KEY.secret" "$SERVER_PRIVATE_KEY"
+    test -f "$SERVER_PUBLIC_CERT.secret" && mv -f "$SERVER_PUBLIC_CERT.secret" "$SERVER_PUBLIC_CERT"
+    test -f "$SERVER_PUBLIC_DN.secret" && mv -f "$SERVER_PUBLIC_DN.secret" "$SERVER_PUBLIC_DN"
+    test -f "$SERVER_SECRETS.secret" && mv -f "$SERVER_SECRETS.secret" "$SERVER_SECRETS"
+else
+    test -f $SERVER_PRIVATE_KEY && grep -c "$PLACEHOLDER" "$SERVER_PRIVATE_KEY" > /dev/null || mv "$SERVER_PRIVATE_KEY" "$SERVER_PRIVATE_KEY.secret"
+    echo "$PLACEHOLDER private key - use docker secret to replace it..." > $SERVER_PRIVATE_KEY
+    SERVER_PRIVATE_KEY="$SERVER_PRIVATE_KEY.secret"
+
+    test -f $SERVER_PUBLIC_CERT && grep -c "$PLACEHOLDER" "$SERVER_PUBLIC_CERT" > /dev/null || mv "$SERVER_PUBLIC_CERT" "$SERVER_PUBLIC_CERT.secret"
+    echo "$PLACEHOLDER public cert - use docker secret to replace it..." > $SERVER_PUBLIC_CERT
+    SERVER_PUBLIC_CERT="$SERVER_PUBLIC_CERT.secret"
+
+    test -f $SERVER_PUBLIC_DN && grep -c "$PLACEHOLDER" "$SERVER_PUBLIC_DN"  > /dev/null || mv "$SERVER_PUBLIC_DN" "$SERVER_PUBLIC_DN.secret"
+    echo "$PLACEHOLDER public FQDN - use docker secret to replace it..." > $SERVER_PUBLIC_DN
+    SERVER_PUBLIC_DN="$SERVER_PUBLIC_DN.secret"
+
+    test -f $SERVER_SECRETS && grep -c "$PLACEHOLDER" "$SERVER_SECRETS"  > /dev/null || mv "$SERVER_SECRETS" "$SERVER_SECRETS.secret"
+    echo "$PLACEHOLDER secrets - use docker secret to replace it..." > $SERVER_SECRETS
+    SERVER_SECRETS="$SERVER_SECRETS.secret"
+fi
+
 if [[ ! -f "$SERVER_SECRETS" ]]; then
     echo "You must create file $SERVER_SECRETS!!!"
-    echo "You can use $SERVER_SECRETS.template ..."
+    echo "You can use ipsec.secrets.template ..."
     exit 3
 fi
 
@@ -60,7 +91,7 @@ if [[ ! -f "$SERVER_PUBLIC_CSR" ]]; then
     echo "Done..."
 fi
 
-openssl req -in server-csr.pem  -noout -subject | sed 's/.*CN *= *\([^, ]*\).*/\1/' > $SERVER_PUBLIC_DN
+openssl req -in $SERVER_PUBLIC_CSR -noout -subject | sed 's/.*CN *= *\([^, ]*\).*/\1/' > $SERVER_PUBLIC_DN
 
 if [[ ! -f "$SERVER_PUBLIC_CERT" ]]; then
     echo "Generating server public certificate ($SERVER_PUBLIC_CERT)..."
@@ -80,7 +111,7 @@ EOF
     echo
 fi
 
-openssl x509 -noout -subject -in server-cert.pem | sed 's/.*CN *= *\([^, ]*\).*/\1/' > $SERVER_PUBLIC_DN
+openssl x509 -noout -subject -in $SERVER_PUBLIC_CERT | sed 's/.*CN *= *\([^, ]*\).*/\1/' > $SERVER_PUBLIC_DN
 
 echo "Building image..."
 docker image build --tag vpn-strongswan:$GIT_VERSION .
@@ -90,14 +121,27 @@ echo
 echo "You should install this CA certificate on your devices ($CA_PUBLIC_CERT):"
 cat $CA_PUBLIC_CERT
 echo
+if [[ "PREFIX$SECRET_PREFIX" == "PREFIX" ]]; then
 echo "In order to run interactively use this command:"
 echo "docker run --read-only --cap-add NET_ADMIN --rm -it -p 4500:4500/udp -p 500:500/udp vpn-strongswan:$GIT_VERSION"
 echo
 echo "In order to run in normal mode use this command:"
 echo "docker run --read-only --cap-add NET_ADMIN -d -p 4500:4500/udp -p 500:500/udp vpn-strongswan:$GIT_VERSION"
 echo
+else
+echo "In order to create secrets use this commands:"
+echo "docker secret create $SECRET_PREFIX_PK $SERVER_PRIVATE_KEY"
+echo "docker secret create $SECRET_PREFIX_PC $SERVER_PUBLIC_CERT"
+echo "docker secret create $SECRET_PREFIX_DN $SERVER_PUBLIC_DN"
+echo "docker secret create $SECRET_PREFIX_SE $SERVER_SECRETS"
+echo
+SECRETS="$SECRETS --secret source=$SECRET_PREFIX_PK,target=/etc/ipsec.d/private/server-key.pem,mode=0400"
+SECRETS="$SECRETS --secret source=$SECRET_PREFIX_PC,target=/etc/ipsec.d/certs/server-cert.pem,mode=0400"
+SECRETS="$SECRETS --secret source=$SECRET_PREFIX_DN,target=/etc/ipsec.d/certs/server-dn.txt,mode=0400"
+SECRETS="$SECRETS --secret source=$SECRET_PREFIX_SE,target=/etc/ipsec.secrets,mode=0400"
+fi
 echo "In order to run as a service use this command:"
-echo "docker service create --read-only --cap-add NET_ADMIN -d -p 4500:4500/udp -p 500:500/udp vpn-strongswan:$GIT_VERSION"
+echo "docker service create --read-only --cap-add NET_ADMIN -d $SECRETS -p 4500:4500/udp -p 500:500/udp vpn-strongswan:$GIT_VERSION"
 echo
 echo "You can change VPN network subnet adding option: -e INTERNAL_SUBNET=\"10.10.10.0/24\""
 echo "You can change DNS servers adding option: -e DNS_ADDRESS=\"8.8.8.8,8.8.4.4\""
